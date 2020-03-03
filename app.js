33546d4ac29582449b355543c336bc0620219455
Amal Madhavan <amalmadhavan@Amals-MacBook.local>
2020-03-03 19:54:37 +0530

    add post route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT | 8000
 
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: false}));
 app.use("/assets/css", express.static(__dirname + "/assets/css"));
 
 app.get('/', function (req, res) {
@@ -16,4 +17,9 @@ app.get('/questions/add', function (req, res) {
 	res.render('addQuestion')
 })
 
-app.listen(port, ()=> {console.log("Hey: " + port)})
+app.post('/questions/add', function (req, res) {
+	console.log(res.body);
+	res.send(req.body);
+})
+
+app.listen(port, ()=> {console.log("Hey:as " + port)})
