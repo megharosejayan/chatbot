f6f66921511df7e119e86a490fe5147cc32eb32d
Jyothis P <jyothisp52@gmail.com>
2020-03-23 22:55:26 +0530

    addMessage Function added to formhandle.js

--- assets/js/formhandle.js
+++ assets/js/formhandle.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   document.getElementById("myText").focus();
 
   $('.msger-chat').scrollTop($('.msger-chat')[0].scrollHeight);
 
 
-  $('form').on('submit', function(){
+  $('form').on('submit', function () {
 
-      var item = $('form input');
-      var chat = {item: item.val()};
+    var item = $('form input');
+    var chat = { item: item.val() };
 
 
-      $.ajax({
-        type: 'POST',
-        url: '/answer',
-        data: chat,
-        success: function(data){
-          
-          location.reload();
-          
-            $('.msger-input').focus();
-        
-        }
-      
-      });
+    $.ajax({
+      type: 'POST',
+      url: '/answer',
+      data: chat,
+      success: function (data) {
 
-      return false;
+        location.reload();
+
+        $('.msger-input').focus();
+
+      }
+
+    });
+
+    return false;
 
   });
 
@@ -33,22 +33,29 @@ $(document).ready(function(){
 });
 
 
-function addBotMessage(message, time){
-  
-  let htmlString = `
-  <div class="msg right-msg">
-    <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)"></div>
+function addMessage(message, time, bot = false) {
 
-    <div class="msg-bubble">
+  let side = bot ? 'left' : 'right';
+  let name = bot ? 'Bot' : 'You';
 
-      <div class="msg-info">
-        <div class="msg-info-name">You</div>
-        <div class="msg-info-time">` + time + `</div>
-      </div>
+  let htmlString = `      <div class="msg ` + side + `-msg">
+  <div class="msg-bubble">
 
-      <div class="msg-text">` + message + `
-      </div>
-
+    <div class="msg-info">
+      <div class="msg-info-name">` + name + `</div>
+      <div class="msg-info-time">` + time + `</div>
     </div>
-  </div>`
+
+    <div class="msg-text">
+    ` + message + `
+    </div>
+
+  </div>
+</div>`;
+
+
+  let chatContainer = $('.msger-chat');
+
+  chatContainer.append(htmlString);
+  chatContainer.scrollTop($('.msger-chat')[0].scrollHeight);
 }
